dc376cbb59e84682c8f5cb756546c9ee55d34a55
Flareonn <appavel2011@yandex.ru>
2020-08-10 20:51:14 +0500

    Pre-start

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+ return (
+  <header>
+   <a href="vk.com/">Home</a>
+   <a href="vk.com/">About us</a>
+   <a href="vk.com/">Contact</a>
+  </header>
+ );
+};
+
+export default Header;
